c15b01ed6d8acdabe9604dceee57438965aa1e30
JeasonCB <camposjeason@gmail.com>
2023-07-13 13:38:42 -0400

    first commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ export default function Home () {
     <main className="">
       <div className="grid h-screen w-full place-content-center  justify-center bg-gray-950 select-none">
         <motion.h1
-          initial={{opacity: 0, scale: 0.5}}
+          initial={{ opacity: 0, scale: 0.5 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 0.5 }}
           className='text-[calc(100vw/7)] lg:text-[calc(10vw)] text-transparent font-extrabold bg-clip-text bg-gradient-to-t from-yellow-400 to-orange-600' >
